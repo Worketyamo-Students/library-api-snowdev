b3510d5512f406f1b809af2d2d06052bf944fd1f
DimitriTedom <dimitritedom@gmail.com>
2025-06-21 18:40:55 +0100

    assigment finished

--- services/userService.ts
+++ services/userService.ts
@@ -2,12 +2,12 @@ import bcrypt from 'bcryptjs';
 import prisma from '../prismaClient';
 export const registerUser = async (nom: string,email: string, motDePasse: string) => {
 
+  if (!nom || !email || !motDePasse) {
+    throw new Error('Tous les champs sont obligatoires : nom, email, motDePasse');
+  }
     const userExist= await prisma.user.findUnique({where: {email}});
     if (userExist) {
      throw new Error("L\'utilisateur existe deja. ");
-    }
-    if (!nom || !email || !motDePasse) {
-      throw new Error('Tous les champs sont obligatoires : nom, email, motDePasse');
     }
     
     const hashedPassword = await bcrypt.hash(motDePasse,10);
